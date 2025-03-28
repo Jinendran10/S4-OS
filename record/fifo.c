5c4ff5c997a6446f2d27334467fdb6e5d0748218
Jinendran10 <jinendranjinu10@gmail.com>
2025-03-28 10:26:17 +0530

    Add files via upload

---
+++ record/fifo.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main(){
+	int frames,l,i,j,k,exist,m[10],str[100],count=0;
+	printf("Enter the length of the reference string:");
+	scanf("%d",&l);
+	printf("Enter the reference string:");
+	for(i=0;i<l;i++)
+		scanf("%d",&str[i]);
+	printf("Enter the no.of partitions:");
+	scanf("%d",&frames);
+	for(i=0;i<frames;i++)
+		m[i]=-1;
+	printf("\nThe page replacement process is...\n");
+	for(i=0;i<l;i++,exist=0){
+		for(j=0;j<frames;j++){
+			if(m[j]==str[i]){
+				exist=1;
+			}
+		}
+		if(exist==0){
+			m[count%frames]=str[i];
+			count++;
+		}
+		else
+			printf("HIT!\n");
+	}
+	printf("\nTotal Page Fault=%d\n",count);
+	printf("Total Hits=%d\n",l-count);
+	printf("Miss Ratio=%d%%\n",(count*100)/l);
+	printf("Hit Ratio=%d%%\n\n",((l-count)*100)/l);
+}
